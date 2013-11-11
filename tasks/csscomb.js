bf27255529472bde18059095bd9c4e2208f1d03a
t32k <ijok.ijok@gmail.com>
2013-11-11 22:56:02 +0900

    Improve process

--- tasks/csscomb.js
+++ tasks/csscomb.js
@@ -10,44 +10,44 @@ module.exports = function (grunt) {
 
   grunt.registerMultiTask('csscomb', 'Sorting CSS properties in specific order.', function () {
 
-    var async = require('async'),
-        Comb = require('csscomb'),
-        defaultConfig = require('../node_modules/csscomb/.csscomb.json'),
-        done = this.async();
+    var Comb = require('csscomb'),
+      defaultConfig = require('../node_modules/csscomb/.csscomb.json');
 
-    async.eachSeries(this.files, function (file, next) {
-      // Get config file from task's options:
-      var config = grunt.task.current.options().sortOrder;
+    // Get config file from task's options:
+    var config = grunt.task.current.options().sortOrder;
 
-      // Check if config file is set and exists. If not, use default one:
-      if (config && grunt.file.exists(config)) {
-        grunt.log.ok('Using custom config file "' + config + '"...');
-        config = grunt.file.readJSON(config);
-      } else {
-        config = defaultConfig;
-      }
+    // Check if config file is set and exists. If not, use default one:
+    if (config && grunt.file.exists(config)) {
+      grunt.log.ok('Using custom config file "' + config + '"...');
+      config = grunt.file.readJSON(config);
+    } else {
+      config = defaultConfig;
+    }
 
-      // Check if source file exists. If not, display a warning:
-      if (file.src.length === 0) {
-        grunt.log.error('Source file "' + file.orig.src + '" not found.');
-        return;
-      }
+    this.files.forEach(function (f) {
 
       // Create a new instance of csscomb and configure it:
       var comb = new Comb();
       comb.configure(config);
 
-      // Get CSS from a source file:
-      var css = grunt.file.read(file.src);
-      // Comb it:
-      grunt.log.ok('Sorting file "' + file.src + '"...');
-      var combed = comb.processString(css);
-      // Write result to a destination file:
-      grunt.file.write(file.dest, combed);
-      grunt.log.ok('OK.');
+      f.src.filter(function (filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).forEach(function (src) {
 
-      // Repeat:
-      next();
-    }, done);
+        // Get CSS from a source file:
+        var css = grunt.file.read(src);
+
+        // Comb it:
+        grunt.log.ok('Sorting file "' + src + '"...');
+        var combed = comb.processString(css);
+        grunt.file.write(f.dest, combed);
+      });
+    });
   });
 };
